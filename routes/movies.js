8e80d29f9e2feb3937c33a859fb07de1e7af3b42
ALowton <ALowton@QA.com>
2021-05-21 14:33:13 +0100

    AL: Cleaner create

--- routes/movies.js
+++ routes/movies.js
@@ -7,16 +7,10 @@ const { NotFound } = require("../errors");
 
 // Post Requests
 ROUTER.post("/create", async(req, res, next) => {
-    const NEW_OBJ = {
-        title: req.body.title,
-        year_released: req.body.year_released,
-        actors: req.body.actors,
-        reviews: req.body.reviews
-    };
-    const DOC = new MOVIE (NEW_OBJ); // New document using model
+    const DOC = new MOVIE (req.body); // New document using model
     try {
-        await DOC.save();
-        res.status(201).send(NEW_OBJ)
+        const CREATED = await DOC.save();
+        res.status(201).send(CREATED);
     } catch(err) {
         next(new Error(err.message)); // Validation from schema provides the appropriate error messages here
     }
@@ -28,17 +22,16 @@ ROUTER.get("/getAll", async(req, res, next) => {
         const MOVIES = await MOVIE.find();
         (MOVIES.length)? res.send(MOVIES) : next(new Error("ERROR: There are no movies to retrieve"));
     } catch(err) {
-        next(new Error("ERROR: Could not retrieve movies!"));
+        next(new Error(err.message));
     }
 });
 
 ROUTER.get("/get/:id", async(req, res, next) => {
     try {
         const FOUND = await MOVIE.findById(req.params.id);
-        if (FOUND) res.send(FOUND);
-        next(new NotFound("ERROR: Could not find a movie with that ID"));
+        (FOUND)? res.send(FOUND) : next(new NotFound("ERROR: Could not find a movie with that ID"));
     } catch(err) {
-        next(new NotFound("ERROR: Could not find a movie with that ID"));
+        next(new Error(err.message));
     }
 });
 
@@ -50,10 +43,9 @@ ROUTER.put("/update/:id", async(req, res, next) => {
             {title: req.query.title},
             {new: true}
         );
-        if (UPDATED) res.status(202).send(UPDATED);
-        next(new NotFound("ERROR: Could not find a movie with that ID"));
+        (UPDATED)? res.status(202).send(UPDATED) : next(new NotFound("ERROR: Could not find a movie with that ID"));
     } catch(err) {
-        next(new NotFound("ERROR: Could not find a movie with that ID"));
+        next(new Error(err.message));
     }
 });
 
@@ -61,10 +53,9 @@ ROUTER.put("/update/:id", async(req, res, next) => {
 ROUTER.delete("/delete/:id", async(req, res, next) => {
     try {
         const DELETED = await MOVIE.findByIdAndDelete(req.params.id);
-        if (DELETED) res.status(204).send();
-        next(new NotFound("ERROR: Could not find a movie with that ID"));
+        (DELETED)? res.status(204).send() : next(new NotFound("ERROR: Could not find a movie with that ID"));
     } catch(err) {
-        next(new NotFound("ERROR: Could not find a movie with that ID"));
+        next(new Error(err.message));
     }
 });
 
